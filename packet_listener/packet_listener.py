b9643d4026eba6bb780d60f0798e09b7a5a8a509
M. Deniz Özkahraman <mehmetdenizozkahraman@outlook.com>
2022-08-05 16:11:39 +0300

    update

--- packet_listener/packet_listener.py
+++ packet_listener/packet_listener.py
@@ -4,10 +4,8 @@ from scapy_http import http
 def listen_packets(interface):
 
     scapy.sniff(iface=interface,store=False,prn=analyze_packets)
-    #prn = callback function
 
 def analyze_packets(packet):
-    #packet.show()
     if packet.haslayer(http.HTTPRequest):
         if packet.haslayer(scapy.Raw):
             print(packet[scapy.Raw].load)
